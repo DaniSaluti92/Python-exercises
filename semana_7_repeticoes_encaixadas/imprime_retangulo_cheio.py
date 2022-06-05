942a9c7e7b407b7fb3e7c5507ec0161a962d47eb
Daniele Saluti <106193671+DaniSaluti92@users.noreply.github.com>
2022-06-05 00:25:31 -0300

    Create imprime_retangulo_cheio.py
    Escreva um programa que recebe como entradas (utilize a função input) dois números inteiros correspondentes à largura e à altura de um retângulo, respectivamente. O programa deve imprimir, usando repetições encaixadas (laços aninhados), uma cadeia de caracteres que represente o retângulo informado com caracteres '#' na saída.

---
+++ semana_7_repeticoes_encaixadas/imprime_retangulo_cheio.py
@@ -0,0 +1,11 @@
+l=int(input("digite a largura:"))
+h=int(input("digite a altura:"))
+
+b=1
+while (b<=h):
+    a=1
+    while (a<=l):
+        print("#", end='')
+        a=a+1
+    print(end="\n")
+    b=b+1
